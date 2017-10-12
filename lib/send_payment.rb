55a547a05685831027241db39b8d4b4cf57042d9
anik3tra0 <anik3tra0@gmail.com>
2017-10-13 01:32:11 +0530

    added SendPayment class with specs

--- lib/send_payment.rb
+++ lib/send_payment.rb
@@ -1,6 +1,29 @@
+require 'digest'
+require 'openssl'
+require 'base64'
+require 'uri'
+require 'net/http'
+require 'json'
+
 class SendPayment
+	ENCRYPTION_KEY = 'Q9fbkBF8au24C9wshGRW9ut8ecYpyXye5vhFLtHFdGjRg3a4HxPYRfQaKutZx5N4'
+	ENCRYPTION_IV = '\x81\xE6$\xA1rc+tk\x98.\xFB\x1EGl\xB1'
+	URL = URI("https://somebank.com/transaction")
+
 	def initialize(transaction)
 		@transaction = transaction
+	end
+
+	def make
+		http = Net::HTTP.new(URL.host, URL.port)
+
+		request = Net::HTTP::Post.new(URL)
+		request["content-type"] = 'application/json'
+		request.body = { msg: encrypted_payload }
+		request.body = request.body.to_json
+		#
+		# response = http.request(request)
+		puts request.body
 	end
 
 	private
@@ -8,4 +31,19 @@ class SendPayment
 	def stringify
 		(@transaction.map { |k, v| "#{k}=#{v}" }).join('|')
 	end
+
+	def payload_with_hash
+		hash = Digest::SHA1.hexdigest(stringify)
+		stringify + "hash=#{hash}"
+	end
+
+	def encrypted_payload
+		cipher = OpenSSL::Cipher::AES128.new(:CBC)
+		cipher.encrypt
+		cipher.key = ENCRYPTION_KEY
+		cipher.iv = ENCRYPTION_IV
+		encrypted_payload = cipher.update(payload_with_hash) + cipher.final
+		Base64.encode64(encrypted_payload)
+	end
+
 end
--- spec/send_payment_spec.rb
+++ spec/send_payment_spec.rb
@@ -1,12 +1,27 @@
 require 'send_payment'
 RSpec.describe SendPayment do
 	context 'with a outward payment request' do
+		
+		before(:each) do
+			transaction = { bank_ifsc_code: 'ANZB0001122', bank_account_number: '1111222233334444', amount: '105000', merchant_transaction_ref: 'txn001', transaction_date: '2017-10-12', payment_gateway_merchant_reference: 'merc001' }
+			@send_payment = SendPayment.new(transaction)
+		end
 
 		describe '#stringify' do
 			it 'creates a pipe seperated string' do
-				transaction = { bank_ifsc_code: 'ANZB0001122', bank_account_number: '1111222233334444', amount: '105000', merchant_transaction_ref: 'txn001', transaction_date: '2017-10-12', payment_gateway_merchant_reference: 'merc001' }
-				send_payment = SendPayment.new(transaction)
-				expect(send_payment.send(:stringify)).to include('bank_ifsc_code', 'bank_account_number', 'amount', 'merchant_transaction_ref', 'transaction_date', 'payment_gateway_merchant_reference', '|')
+				expect(@send_payment.send(:stringify)).to include('bank_ifsc_code', 'bank_account_number', 'amount', 'merchant_transaction_ref', 'transaction_date', 'payment_gateway_merchant_reference', '|')
+			end
+		end
+
+		describe '#payload_with_hash' do
+			it 'creates a hash of the pipe seperated string and appends it to the same string ' do
+				expect(@send_payment.send(:payload_with_hash)).to include('bank_ifsc_code', 'bank_account_number', 'amount', 'merchant_transaction_ref', 'transaction_date', 'payment_gateway_merchant_reference', '|', 'hash')
+			end
+		end
+
+		describe '#encrypted_payload' do
+			it 'encrypts the pipe seperated string encodes it using base64' do
+				expect(@send_payment.send(:encrypted_payload)).to match(/^(?:[A-Za-z0-9+\/]{4}\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/)
 			end
 		end
 
